ae3e031ad3089cff940fa65af388df6193373579
pavan0062 <143992297+pavan0062@users.noreply.github.com>
2023-09-25 12:28:45 +0530

    Create staticBlock1.java

---
+++ staticBlock1.java
@@ -0,0 +1,9 @@
+//Java programm to demonstrate the static block
+//25-09-2023
+//Staticblock1.java
+public class A2{  
+  static{System.out.println("static block is invoked");}  
+  public static void main(String args[]){  
+   System.out.println("inside the main method");  
+  }  
+} 
