d40587a703ed4da3f5797b9650fe5e8a409069d7
pavan0062 <143992297+pavan0062@users.noreply.github.com>
2023-09-25 12:15:21 +0530

    Create CheckPalindrome.java

---
+++ CheckPalindrome.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+  public class CheckPalindrome
+  {
+      public static void main(String args[])
+      {
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Enter a positive integer to check whether it is palindrome or not");
+          int n = sc.nextInt();
+          int temp = n;
+          int rev = 0;
+          while(n>0)
+          {
+              int rem = n%10;
+              rev = (rev*10)+rem;
+              n = n/10;
+          }
+          if(temp==rev)
+          {
+              System.out.println("Temp " + "is a palindrome");
+          }
+          else
+          {
+              System.out.println("Temp " + "is not a palindrome");
+          }
+      }
+  }
